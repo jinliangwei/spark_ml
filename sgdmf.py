77458cb2d2033f6f912224996f6dc2001b5256d3
Andrew Chung <afchung90@gmail.com>
2016-09-03 14:21:57 -0400

    Cartesian product to replace repartition-shuffle

--- sgdmf.py
+++ sgdmf.py
@@ -8,7 +8,6 @@ import math
 
 def get_block_dim(num_ids, num_blocks, block_id):
     cut_off_block = num_ids % num_blocks
-    block_dim = 0
     if block_id < cut_off_block:
         block_dim = num_ids / num_blocks + 1
     else:
@@ -17,7 +16,6 @@ def get_block_dim(num_ids, num_blocks, block_id):
 
 def get_block_offset(num_ids, num_blocks, block_id):
     cut_off_block = num_ids % num_blocks
-    block_offset = 0
     if block_id <= cut_off_block:
         block_offset = (num_ids / num_blocks + 1) * block_id
     else:
@@ -29,7 +27,6 @@ def get_block_id(num_ids, num_blocks, my_id):
     cut_off_block = num_ids % num_blocks
     cut_off_block_offset = (num_ids / num_blocks + 1) * cut_off_block
 
-    block_id = 0
     if my_id >= cut_off_block_offset:
         block_id = (my_id - cut_off_block_offset) / (num_ids / num_blocks) + cut_off_block
     else:
@@ -54,17 +51,6 @@ def blockify_matrix(partition_id, partition, num_y_ids, num_blocks):
 def initialize_factor_matrix(block_id, K, block_dim):
     factor_block = np.random.rand(block_dim, K)
     return block_id, factor_block
-
-
-def initialize_factor_matrix2(block_id, K, block_dim):
-    random.seed(1) # always use the same seed to get deterministic results
-    factor_block = []
-    for i in range(0, block_dim):
-        row = []
-        for j in range(0, K):
-            row.append(random.random())
-        factor_block.append(row)
-    return block_id, np.array(factor_block)
 
 def sgd_on_one_partition(partition, sub_epoch, num_x_ids, num_y_ids, num_workers, step_size):
     # partition is of this form: (block_id, ((data, H_block), W_block))
@@ -95,15 +81,15 @@ def sgd_on_one_partition(partition, sub_epoch, num_x_ids, num_y_ids, num_workers
         H_block[H_id] -= step_size * H_gradient
     return block_x_id, (partition[1][0][0], W_block, H_block)
 
-def evaluate_on_one_partition(partition, num_workers, sub_epoch, num_x_ids, num_y_ids):
-    block_x_id = partition[0]
-    block_y_id = (block_x_id + sub_epoch) % num_workers
-    W_block = partition[1][0][1]
-    H_block = partition[1][1]
+def evaluate_error(cart_prod, num_workers, num_x_ids, num_y_ids, error_accum, count_accum):
+    block_x_id = cart_prod[0][0]
+    block_y_id = cart_prod[1][0] % num_workers
+    W_block = cart_prod[0][1][1]
+    H_block = cart_prod[1][1]
 
-    if not block_y_id in partition[1][0][0]:
+    if not block_y_id in cart_prod[0][1][0]:
         return 0, 0
-    data_block = partition[1][0][0][block_y_id]
+    data_block = cart_prod[0][1][0][block_y_id]
 
     x_id_offset = get_block_offset(num_x_ids, num_workers, block_x_id)
     y_id_offset = get_block_offset(num_y_ids, num_workers, block_y_id)
@@ -118,103 +104,82 @@ def evaluate_on_one_partition(partition, num_workers, sub_epoch, num_x_ids, num_
         error += diff ** 2
         n += 1
 
-    return error, n
-
-def accumulate_error(partition, num_workers, sub_epoch, num_x_ids, num_y_ids, \
-                     error_accum, count_accum):
-    error, count = evaluate_on_one_partition(partition, num_workers, sub_epoch, num_x_ids, num_y_ids)
     error_accum.add(error)
-    count_accum.add(count)
+    count_accum.add(n)
 
 if __name__ == "__main__":
     csv_file = sys.argv[1]
     K = int(sys.argv[2]) #rank
     num_workers = int(sys.argv[3])
     num_iterations = int(sys.argv[4])
-    temp_file = sys.argv[5]
     step_size = 0.001
     step_size_decay = 0.99
 
     conf = pyspark.SparkConf().setAppName("DSGD-MF")
     sc = pyspark.SparkContext(conf=conf)
+
     ratings = sc.textFile(csv_file).map(parse_line)
+
     num_x_ids = ratings.map(lambda x: x[0]).max() + 1
     num_y_ids = ratings.map(lambda x: x[1]).max() + 1
 
     print 'num is ', num_x_ids, num_y_ids
 
-    blockified_ratings = ratings.map(lambda x: (get_block_id(num_x_ids, num_workers, x[0]), x))\
+    blockified_ratings = ratings.map( \
+        lambda triple: (get_block_id(num_x_ids, num_workers, triple[0]), triple))\
       .partitionBy(num_workers) \
-      .mapPartitionsWithIndex(lambda x, y: blockify_matrix(x, y, num_y_ids, num_workers), \
-                              preservesPartitioning=True)
+      .mapPartitionsWithIndex( \
+        lambda partition_id, partition: blockify_matrix(partition_id, partition, num_y_ids, num_workers), \
+        preservesPartitioning=True)
 
     print "blockified ratings"
 
-    #blockified_ratings.saveAsTextFile(csv_file + ".blockified_ratings")
     block_ids = sc.parallelize([x for x in range(num_workers)]).persist()
 
+    # <block_id, W_block_mat>
     W_matrix = block_ids.map(lambda x: initialize_factor_matrix(x, K, \
                                                                 get_block_dim(num_x_ids, num_workers, x)))
+
+    # {idx, H_mat}
     H_matrices = {}
     H_matrices[0] = block_ids.map(lambda x: initialize_factor_matrix(x, K, \
                                                                      get_block_dim(num_y_ids, num_workers, x)))
-
-#    W_matrix.map(lambda x: (x[0], x[1].tolist())).saveAsTextFile(csv_file + ".W_matrix")
-#    H_matrices[0].map(lambda x: (x[0], x[1].tolist())).saveAsTextFile(csv_file + ".H_matrix")
-
-
+    # {idx, <block_id, <block_ratings, W_block_mat>>}
     partitioned_data_with_Ws = {}
     partitioned_data_with_Ws[0] = blockified_ratings.join(W_matrix, numPartitions=num_workers)
-#    partitioned_data_with_Ws[0] = blockified_ratings.join(W_matrix)
-#    partitioned_data_with_Ws[0].saveAsTextFile(temp_file + ".partitioned_data_with_Ws")
-    updated_joined_data = {}
-    eval_remapped_Hs = {}
 
     def perform_sgd_on_one_partition(sub_epoch, num_x_ids, num_y_ids, num_workers, step_size):
         return (lambda x: sgd_on_one_partition(x, sub_epoch, num_x_ids, num_y_ids, num_workers, step_size))
 
     def update_H_matrix_ids(num_workers):
-        return (lambda x: ((x[0] + num_workers - 1) % num_workers, x[1][2]))
-
-    def remapp_H_ids(sub_epoch, num_workers):
-        return (lambda x: ((x[0] + num_workers - sub_epoch) % num_workers, x[1]))
-
-    def perform_accumulate_error(num_workers, sub_epoch, num_x_ids, \
-                                 num_y_ids, error_accum, count_accum):
-        return (lambda x: accumulate_error(x, num_workers, sub_epoch, num_x_ids, \
-                                        num_y_ids, error_accum, count_accum))
+        return (lambda x: ((x[0] - 1) % num_workers, x[1][2]))
 
     start_time = time.time()
     print("iteration\ttime (sec)\terror\trmse\tcount")
     rmses = []
     for iteration in range(num_iterations):
+        error_accum = sc.accumulator(0.0)
+        count_accum = sc.accumulator(0)
         for sub_epoch in range(num_workers):
-            index = iteration * num_workers + sub_epoch
-
-            updated_joined_data[index] = partitioned_data_with_Ws[index]\
-              .join(H_matrices[index], num_workers)\
+            updated_joined_data = partitioned_data_with_Ws[sub_epoch]\
+              .join(H_matrices[sub_epoch], num_workers)\
               .map(perform_sgd_on_one_partition(sub_epoch, num_x_ids, num_y_ids, \
                                             num_workers, step_size),\
                    preservesPartitioning=True)
 
-            partitioned_data_with_Ws[index + 1] = updated_joined_data[index]\
+            next_index = (sub_epoch + 1) % num_workers
+            partitioned_data_with_Ws[next_index] = updated_joined_data\
               .map(lambda x: (x[0], (x[1][0], x[1][1])),\
                    preservesPartitioning=True)
 
-            H_matrices[index + 1] = updated_joined_data[index]\
+            H_matrices[next_index] = updated_joined_data\
               .map(update_H_matrix_ids(num_workers))
 
-        error_accum = sc.accumulator(0.0)
-        count_accum = sc.accumulator(0)
-        for sub_epoch in range(num_workers):
-            index = iteration * num_workers + sub_epoch
-            eval_remapped_Hs[index] = H_matrices[iteration * num_workers + num_workers]\
-              .map(remapp_H_ids(sub_epoch, num_workers))
-
-            partitioned_data_with_Ws[iteration * num_workers + num_workers]\
-              .join(eval_remapped_Hs[index], num_workers) \
-              .foreach(perform_accumulate_error(num_workers, sub_epoch, num_x_ids, \
-                                            num_y_ids, error_accum, count_accum))
+            if next_index == 0:
+               partitioned_data_with_Ws[0]\
+                       .cartesian(H_matrices[0])\
+                       .foreach(lambda x: evaluate_error(x, num_workers, num_x_ids, \
+                                                    num_y_ids, error_accum, count_accum))
 
         error_total = error_accum.value
         n_total = count_accum.value
